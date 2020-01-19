55c597f5d4e91c5c90d7b6cf76a2c37c6f889aec
Hamdi Atacan Ogul <hamdiatacanogul@gmail.com>
2020-01-19 23:20:14 +0300

    spring boot setup

---
+++ src/test/java/com/atacan/hashpass/HashPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atacan.hashpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
